0fb3e317ebd5bc086564af582b1e0029ef40eff3
lishiwei <15240275367@163.com>
2018-03-07 21:06:04 +0800

    置顶新闻组件设计

---
+++ toutiao/src/components/common/hotNews.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="hotNews">
+        <el-row>
+            <el-col :span="col1">
+                <div class="title">
+                    <div>{{title}}</div>
+                </div>
+                <div class="author" style="clear:both;">
+                    <el-tag type="danger" size="mini">置顶</el-tag>&nbsp;&nbsp;&nbsp;
+                    <span>{{publisher}}</span>&nbsp;&nbsp;<span>{{recommend}}评论</span>
+                </div>
+            </el-col>
+            <el-col :span="col2">
+                <img :src="headImg" alt="headImg" width="120" height="80">
+            </el-col>
+        </el-row>
+        <br>
+        <el-row>
+            <el-col :span="col1">
+                <div class="title">
+                    <div>{{title}}</div>
+                </div>
+                <div class="author" style="clear:both;">
+                    <el-tag type="danger" size="mini">置顶</el-tag>&nbsp;&nbsp;&nbsp;
+                    <span>新华网</span>&nbsp;&nbsp;<span>62评论</span>
+                </div>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+    //置顶新闻，只放置政治信息
+    export default {
+        name: "hotNews",
+        data() {
+            return {
+                title: "习近平：发展是第一要务，人才是第一资源，创新是第一动力",
+                headImg: "../../../static/img/headImg.jpg",
+                col1:16,
+                col2:8,
+                publisher:"新华社",
+                recommend:62
+            }
+        }
+    }
+</script>
+
+<style>
+    .hotNews {
+        clear: both;
+        width: 97%;
+        height: 90px;
+        margin: 0px;
+        padding-top: 10px;
+        margin-top: -10px;
+        text-align: left;
+        border-bottom: 1px solid #CCCCCC
+    }
+    .title {
+        font-family: 'Microsoft YaHei'
+    }
+    .author {
+        margin-top: 10px;
+        font-size: 10px;
+    }
+</style>
--- toutiao/src/components/common/topNavigation.vue
+++ toutiao/src/components/common/topNavigation.vue
@@ -1,34 +1,50 @@
 <template>
-  <div class="topNavigation">
-    <el-tabs v-model="activeName" style="margin-left:20px;">
-        <el-tab-pane label="关注" name="first">关注</el-tab-pane>
-        <el-tab-pane label="推荐" name="second">推荐</el-tab-pane>
-        <el-tab-pane label="南京" name="third">南京</el-tab-pane>
-        <el-tab-pane label="视频" name="fourth">视频</el-tab-pane>
-        <el-tab-pane label="热点" name="fifth">热点</el-tab-pane>
-        <!-- <el-tab-pane label="新时代" name="sixth">新时代</el-tab-pane>
-        <el-tab-pane name="seventh">视频</el-tab-pane> -->
-    </el-tabs>
-    <i class="iconfont icon-liebiao"></i>
-  </div>
+    <div class="topNavigation">
+        <div >
+            <el-tabs v-model="activeName" style="margin-left:15px;">
+                <el-tab-pane label="关注" name="first">
+                    
+                </el-tab-pane>
+                <el-tab-pane label="推荐" name="second">
+                    <hot-news></hot-news>
+                </el-tab-pane>
+                <el-tab-pane label="南京" name="third">
+
+                </el-tab-pane>
+                <el-tab-pane label="视频" name="fourth">
+
+                </el-tab-pane>
+                <el-tab-pane label="热点" name="fifth">
+
+                </el-tab-pane>
+                <!-- <el-tab-pane label="新时代" name="sixth">新时代</el-tab-pane> -->
+                <!-- <el-tab-pane name="seventh">视频</el-tab-pane> -->
+            </el-tabs>
+        </div>
+        <i class="iconfont icon-liebiao" style="position:absolute;left:360px;top:72px"></i>
+    </div>
 </template>
 
 <script>
-// 顶部导航栏
-export default {
-    name:"topNavigation",
-    data(){
-        return {
-            msg:"这是顶部导航栏",
-            activeName:"second"
+    // 顶部导航栏
+    import hotNews from '@/components/common/hotNews'
+    export default {
+        name: "topNavigation",
+        data() {
+            return {
+                msg: "这是顶部导航栏",
+                activeName: "second"
+            }
+        },
+        components:{
+            hotNews
         }
     }
-}
 </script>
 
 <style>
     .topNavigation {
-        margin:0px;
+        margin: 0px;
         padding: 0px;
         width: 100%;
         height: 40px;
